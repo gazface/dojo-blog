f4d25e5fb3893c6731f17d73d6c56e378b9c1e8b
Gaz Thistlethwaite <gazface@gmail.com>
2021-08-06 22:29:06 +0100

    Lesson 4

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
 
 function App() {
+  const title = 'Welcome to Dojo Blog';
+  const likes = 50;
+  const link = "http://www.google.co.uk";
+
   return (
     <div className="App">
-      Dojo-Blog
+      <div className="content">
+        <h1>{ title }</h1>
+        <p>Liked { likes } times</p>
+        <p>{ 10 }</p>
+        <p>{ "hello, ninjas" }</p>
+        <p>{ [1,2,3,4,5] }</p>
+        <p>{ Math.random() * 10 }</p>
+
+        <a href={ link }>Google</a>
+      </div>
     </div>
   );
 }
