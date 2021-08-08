a28cc0cf92571cfdbb0cd84fa57655189761b0b2
Gaz Thistlethwaite <gazface@gmail.com>
2021-08-08 21:56:28 +0100

    Lesson 15

--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,32 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
-import image from "../src/img/Bloggers and influencers writing articles.jpg";
+import './index.css';
 
 const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-      ])
+      ]);
+
+      const [name, setName] = useState('mario');
 
       const handleDelete = (id) => {
           const newBlogs = blogs.filter(blog => blog.id !== id);
           setBlogs(newBlogs);
       }
 
+// Dependency array
+      useEffect(() => {
+        console.log('use effect ran');
+        console.log(name);
+      }, [name]);
+
     return ( 
         <div className="home">
-            <div className="div heroImage">
-        <img src={image} height={500} width={750} alt="bloggers blogging"/>
-      </div>
             <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+            <button onClick={() => setName('luigi')}>change name</button>
+            <p>{name}</p>
         </div>
      );
 }
