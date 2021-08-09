bba448cd157fe80f1d836d08363985e36c452103
Gaz Thistlethwaite <gazface@gmail.com>
2021-08-09 23:46:41 +0100

    Lesson 27

--- src/Home.js
+++ src/Home.js
@@ -1,13 +1,11 @@
 import BlogList from "./BlogList";
-import useFetch from './useFetch';
+import useFetch from "./useFetch";
 
 const Home = () => {
-
-    const { data: blogs, isPending, error } = useFetch ('http://localhost:8000/blogs'); 
+  const { error, isPending, data: blogs } = useFetch('http://localhost:8000/blogs')
 
   return (
     <div className="home">
-    <h2>All blogs</h2>
       { error && <div>{ error }</div> }
       { isPending && <div>Loading...</div> }
       { blogs && <BlogList blogs={blogs} /> }
